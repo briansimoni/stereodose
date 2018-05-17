59ac2fce7e2da8988725e56f3f1bab5fd87db9c8
Brian Simoni <simonibc@vcu.edu>
2018-05-16 20:21:47 -0400

    Add docker files

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,16 @@
-FROM golang
+FROM golang:1.10
+
+ADD . /go/src/github.com/briansimoni/stereodose
+
+WORKDIR /go/src/github.com/briansimoni/stereodose
+
+RUN go get -u github.com/golang/dep/cmd/dep
+
+RUN dep ensure
+
+RUN go install
+
+# Only for dev purposes
+RUN go get github.com/codegangsta/gin
+
+CMD stereodose
--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/briansimoni/stereodose/app"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "4000"
+	}
 	stereodose := app.InitApp()
-	log.Fatal(http.ListenAndServe(":4000", stereodose))
+	log.Println("Starting stereodose app on port", port)
+	log.Fatal(http.ListenAndServe(":"+port, stereodose))
 }
