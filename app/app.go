edd0ddd532d3bc111965f37aa703e8b395a30d16
Brian Simoni <brian.simoni1@ge.com>
2018-05-17 13:13:54 -0400

    LoggingHandler

--- app/app.go
+++ app/app.go
@@ -3,8 +3,10 @@ package app
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/briansimoni/stereodose/app/auth"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/sessions"
 )
@@ -17,10 +19,18 @@ func loggedIn(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "login success")
 }
 
+// func logger() func(http.Handler) http.Handler {
+// 		handlers.CombinedLoggingHandler(os.Stdout, next)
+// 		next.ServeHTTP()
+// }
+
 func InitApp() *mux.Router {
 	var store = sessions.NewCookieStore([]byte("something-very-secret"))
 
 	app := mux.NewRouter()
+	app.Use(func(next http.Handler) http.Handler {
+		return handlers.LoggingHandler(os.Stdout, next)
+	})
 
 	authRouter := app.PathPrefix("/auth").Subrouter()
 	auth.RegisterHandlers(store, authRouter)
