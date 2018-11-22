2b0ce9a460c7d2543dcb9df316204e03f456c500
briansimoni <simonibc@vcu.edu>
2018-11-21 21:56:13 -0500

    Update categories

--- app/models/categories.go
+++ app/models/categories.go
@@ -8,9 +8,9 @@ type categories map[string][]string
 // ... we could probably do this step on the database layer, but it works fine here
 var Categories = categories{
 	"weed":    []string{"chill", "groovin", "thug life"},
-	"ecstacy": []string{"clouds", "unicorns", "rainbows"},
-	"shrooms": []string{"mario", "luigi", "wario"},
-	"lsd":     []string{"trippy1", "trippy2", "trippy3"},
+	"ecstacy": []string{"dance", "floored", "rolling balls"},
+	"shrooms": []string{"matrix", "shaman", "space"},
+	"lsd":     []string{"calm", "trippy", "fubar", "rockstar"},
 }
 
 func (c categories) Valid(category, subcategory string) bool {
