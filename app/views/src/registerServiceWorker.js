f130aa9da6656a78423de632a76c37a625d35fce
briansimoni <simonibc@vcu.edu>
2018-11-22 10:01:44 -0500

    Modified manifest

--- app/views/src/registerServiceWorker.js
+++ app/views/src/registerServiceWorker.js
@@ -19,7 +19,9 @@ const isLocalhost = Boolean(
 );
 
 export default function register() {
-  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+  console.log(process.env);
+  // if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+    if ('serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location);
     if (publicUrl.origin !== window.location.origin) {
