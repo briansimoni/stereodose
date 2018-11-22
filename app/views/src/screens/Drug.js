20f0975008cb4684fb26da8fe70b878a8b763f8b
briansimoni <simonibc@vcu.edu>
2018-11-22 12:07:10 -0500

    Handle 404's better

--- app/views/src/screens/Drug.js
+++ app/views/src/screens/Drug.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import "./Screens.css";
+import NoMatch from "../404";
+import { Route } from 'react-router-dom';
 
 // Drug renders the mood choices for the chosen Drug
 // Weed -> Chill, Groovin, Thug Life
@@ -11,44 +13,47 @@ class Drug extends React.Component {
   }
 
   render() {
+    let drug = this.props.match.params.drug;
     let match = this.props.match;
-      if (this.state.categories !== null && !this.state.loading) {
-        return (
-          <div className="row">
-            <div className="col">
-              <h2 className="mood-choice-header">Choose Your Mood</h2>
-              <ul id="moods" className="moods">
-                {this.state.categories.map( (category, index) => 
-                  <li key={index}>
-                    <h4><Link to={`${match.url}/${category}`}>{category}</Link></h4>
-                  </li>
-                )}
-              </ul>
-            </div>
+    if (this.state.categories !== null && !this.state.loading && !(drug in this.state.categories)) {
+      return <Route component={NoMatch}/>
+    }
+    if (this.state.categories !== null && !this.state.loading) {
+      return (
+        <div className="row">
+          <div className="col">
+            <h2 className="mood-choice-header">Choose Your Mood</h2>
+            <ul id="moods" className="moods">
+              {this.state.categories[drug].map( (category, index) => 
+                <li key={index}>
+                  <h4><Link to={`${match.url}/${category}`}>{category}</Link></h4>
+                </li>
+              )}
+            </ul>
           </div>
-        )
-      }
+        </div>
+      )
+    }
 
     return (
       <div>Loading...</div>
     )
   }
 
-  componentDidMount() {
-    fetch("/api/categories/", { credentials: "same-origin"} )
-      .then((response) => {
-        return response.json();
-      })
-      .then((json) => {
-        let drug = this.props.match.params.drug;
-        let categories = json[drug];
-        this.setState({ loading: false, categories: categories });
-      })
-      .catch((err) => {
-        console.log(err);
-        this.setState({ loading: false, error: err });
-      })
+  async componentDidMount() {
+    try {
+      const response = await fetch("/api/categories/", { credentials: "same-origin"} );
+      if (response.status !== 200) {
+        throw new Error(response.status + ": Error fetching categories")
+      }
+      const categories = await response.json();
+      this.setState({ loading: false, categories: categories });
+    } catch (err) {
+      console.log(err);
+      this.setState({ loading: false, error: err });
+    }
   }
+
 }
 
 export default Drug;
