555a040026141c7530a783ba350430034f005a0e
briansimoni <simonibc@vcu.edu>
2018-12-16 21:08:23 -0500

    Refactor to async in Playlist.js

--- app/views/src/screens/Playlist.js
+++ app/views/src/screens/Playlist.js
@@ -45,7 +45,8 @@ class Playlist extends React.Component {
     }
   }
 
-  playSong(context, uri) {
+  // playSong makes an API call directly to Spotify
+  async playSong(context, uri) {
     let data = {
       "context_uri": context,
       "offset": {
@@ -53,45 +54,54 @@ class Playlist extends React.Component {
       }
     }
 
-    this.props.getDeviceID().then( (deviceID) => {
-      this.props.getAccessToken().then( (accessToken) => {
-        fetch(`https://api.spotify.com/v1/me/player/play?device_id=${deviceID}`,{
-      
-          method: "PUT",
-          headers : {
-            "Authorization": `Bearer ${accessToken}`,
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify(data)
-        })
-        .catch( (error) => {
-          console.error(error);
-        })
-      })
-    })
+    try {
+      const deviceID = await this.props.getDeviceID();
+      const accessToken = await this.props.getAccessToken();
+
+      const response = await fetch(`https://api.spotify.com/v1/me/player/play?device_id=${deviceID}`, {
+        method: "PUT",
+        headers : {
+          "Authorization": `Bearer ${accessToken}`,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+      });
+
+      if (response.status < 200 || response.status >= 300) {
+        const errorMessage = await response.text();
+        throw new Error(`${errorMessage}, ${response.status}, ${response.statusText}`);
+      }
+    } catch(err) {
+      alert(err.message);
+    }
   }
 
-  componentDidMount() {
+  // grab the data for the particular playlist that the user requested
+  // it can be used later to render a page populated with tracks
+  async componentDidMount() {
+    try {
+      let playlistID = this.props.match.params.playlist
 
-    let playlistID = this.props.match.params.playlist
-    fetch(`/api/playlists/${playlistID}`, { credentials: "same-origin" })
-    .then( (response) => {
-      return response.json();
-    })
-    .then( (json) => {
+      const response = await fetch(`/api/playlists/${playlistID}`, { credentials: "same-origin" });
+      if (response.status !== 200) {
+        const errorMessage = await response.text();
+        throw new Error(`${errorMessage}, ${response.status}, ${response.statusText}`)
+      }
+
+      const json = await response.json();
+
       this.setState({
         loading: false,
         playlist: json
       });
-    })
-    .catch( (err) => {
-      console.log(err);
+    } catch (err) {
       this.setState({
         loading: false,
         error: err
-      });
-    })
+      })
+    }
   }
+
 }
 
 // lets have this component have some function for getting the access token
