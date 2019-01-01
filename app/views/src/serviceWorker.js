255314233519569fc315fcc35302a921fabb64eb
briansimoni <simonibc@vcu.edu>
2019-01-01 01:39:33 -0500

    Fix PWA

--- app/views/src/serviceWorker.js
+++ app/views/src/serviceWorker.js
@@ -24,7 +24,6 @@ export function register(config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
-    console.log(process.env.PUBLIC_URL);
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
