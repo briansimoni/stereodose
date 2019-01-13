e472dd0892dafd2b98b8f8157ad1bf09a0dc4970
briansimoni <simonibc@vcu.edu>
2019-01-13 14:07:57 -0500

    Add browser detection

--- app/views/src/index.js
+++ app/views/src/index.js
@@ -3,6 +3,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { detect } from 'detect-browser';
+
+const browser = detect();
+// handle the case where we don't detect the browser
+if (browser) {
+  console.log(browser.name);
+  console.log(browser.version);
+  console.log(browser.os);
+  const notSupportedMessage = 'Spotify does not work on this os/browser. Try using Chrome. See https://developer.spotify.com/documentation/web-playback-sdk/#supported-browsers';
+  if (browser.os === 'Android OS' && browser.name === 'firefox') {
+    alert(notSupportedMessage);
+  }
+
+  if (browser.name === 'safari') {
+    alert(notSupportedMessage);
+  }
+  if (browser.os === 'iOS' && browser.name === 'chrome') {
+    alert(notSupportedMessage);
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
