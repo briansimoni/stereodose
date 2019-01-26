266569e471a12fc81fdafd74f8a00747407e5fc8
Brian Simoni <simonibc@vcu.edu>
2019-01-26 14:19:11 -0500

    Update service worker

--- app/views/public/sw.js
+++ app/views/public/sw.js
@@ -1,14 +1,14 @@
 const CACHE_NAME = 'stereodose-cache';
 const urlsToCache = [
   '/manifest.json',
-  '/sw.js'
+  '/'
 ];
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
   // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(function(cache) {
+      .then(function (cache) {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
@@ -16,26 +16,13 @@ self.addEventListener('install', function(event) {
 });
 
 
-self.addEventListener('fetch', function(event) {
-  let path = new URL(event.request.url).pathname;
-  if (path === '/' && !navigator.onLine) {
-    event.respondWith(new Promise( function(resolve, reject) {
-      const body = new Blob(['You need to be online for this app to work'], {type : 'text/html'});
-      const res = new Response(body, {status: 200, statusText: 'OK'});
-      resolve(res);
-    }));
-    return;
-  }
-
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        } 
-        return fetch(event.request);
-      }
-    )
-  );
+self.addEventListener('fetch', function (event) {
+  // https://jakearchibald.com/2014/offline-cookbook/#network-falling-back-to-cache
+  event.respondWith(async function () {
+    try {
+      return await fetch(event.request);
+    } catch (err) {
+      return caches.match(event.request);
+    }
+  }());
 });
