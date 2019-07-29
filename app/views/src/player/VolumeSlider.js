f3a97812bbbfd17da456f26a7bba94cad213ef39
munsonwf <munsonwf@gmail.com>
2019-07-29 18:50:09 -0400

    fixed logic to disabled slider (#45)
    * fixed logic to disabled slider
    
    NINJA EDIT
    remove comments
    
    * remove links (#47)
    
    * fix app crash on empty playlist (#46)
    
    * conditionally render tracklist
    
    * Fix typos

--- app/views/src/player/VolumeSlider.js
+++ app/views/src/player/VolumeSlider.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React from 'react';
 import Slider, { Rail, Handles, Tracks } from 'react-compound-slider';
-import Octicon from "react-octicon";
+import Octicon from 'react-octicon';
 
 const railStyle = {
   position: 'absolute',
@@ -8,55 +8,44 @@ const railStyle = {
   height: 10,
   marginTop: 35,
   borderRadius: 5,
-  backgroundColor: '#8B9CB6',
-}
+  backgroundColor: '#8B9CB6'
+};
 
-const defaultValues = [100]
+const defaultValues = [50];
 
 class VolumeSlider extends React.Component {
-
   state = {
     values: defaultValues,
-    update: defaultValues,
-  }
+    update: defaultValues
+  };
 
   onUpdate = update => {
     // nothing right now
-  }
+  };
 
   onChange = values => {
     // values is a set of percentages
     // the web SDK takes a value from 0 - 1
     this.props.onChangeVolume(values[0] / 100);
-  }
+  };
 
   render() {
     const values = this.state.values;
-    const className = this.props.className;
+    const { className, disabled } = this.props;
 
     return (
-      <Slider
-        className={className}
-        domain={[0, 100]}
-        values={values}
-        onChange={this.onChange}
-        disabled={this.props.disabled}
-      >
-      <Octicon className="unmute" name="unmute"/>
+      <Slider className={className} domain={[0, 100]} values={values} onChange={this.onChange} disabled={disabled}>
+        <Octicon className="unmute" name="unmute" />
         <Rail>
-          {({ getRailProps }) => (  // adding the rail props sets up events on the rail
-            <div style={railStyle} {...getRailProps()} />
-          )}
+          {(
+            { getRailProps } // adding the rail props sets up events on the rail
+          ) => <div style={railStyle} {...getRailProps()} />}
         </Rail>
         <Handles>
           {({ handles, getHandleProps }) => (
             <div className="slider-handles">
               {handles.map(handle => (
-                <Handle
-                  key={handle.id}
-                  handle={handle}
-                  getHandleProps={getHandleProps}
-                />
+                <Handle key={handle.id} handle={handle} getHandleProps={getHandleProps} disabled={disabled} />
               ))}
             </div>
           )}
@@ -65,27 +54,21 @@ class VolumeSlider extends React.Component {
           {({ tracks, getTrackProps }) => (
             <div className="slider-tracks">
               {tracks.map(({ id, source, target }) => (
-                <Track
-                  key={id}
-                  source={source}
-                  target={target}
-                  getTrackProps={getTrackProps}
-                />
+                <Track key={id} source={source} target={target} getTrackProps={getTrackProps} disabled={disabled} />
               ))}
             </div>
           )}
         </Tracks>
       </Slider>
-
-    )
+    );
   }
-
 }
 
-
-function Handle({ // your handle component
+function Handle({
+  // your handle component
   handle: { id, value, percent },
-  getHandleProps
+  getHandleProps,
+  disabled
 }) {
   return (
     <div
@@ -99,19 +82,18 @@ function Handle({ // your handle component
         height: 15,
         border: 0,
         textAlign: 'center',
-        cursor: 'pointer',
+        cursor: disabled ? 'not-allowed' : 'pointer',
         borderRadius: '50%',
         backgroundColor: 'green',
-        color: '#333',
+        color: '#333'
       }}
       {...getHandleProps(id)}
-    >
-    </div>
-  )
+    />
+  );
 }
 
-
-function Track({ source, target, getTrackProps }) { // your own track component
+function Track({ source, target, getTrackProps, disabled }) {
+  // your own track component
   return (
     <div
       style={{
@@ -121,13 +103,13 @@ function Track({ source, target, getTrackProps }) { // your own track component
         marginTop: 35,
         backgroundColor: '#145814',
         borderRadius: 5,
-        cursor: 'pointer',
+        cursor: disabled ? 'not-allowed' : 'pointer',
         left: `${source.percent}%`,
-        width: `${target.percent - source.percent}%`,
+        width: `${target.percent - source.percent}%`
       }}
       {...getTrackProps()} // this will set up events if you want it to be clickeable (optional)
     />
-  )
+  );
 }
 
 export default VolumeSlider;
