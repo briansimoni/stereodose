d811a053590ae94c2eb22c019e4ff50835328a62
briansimoni <simonibc@vcu.edu>
2019-11-01 19:03:23 -0400

    Move volume slider styles to their own file

--- app/views/src/player/VolumeSlider.js
+++ app/views/src/player/VolumeSlider.js
@@ -1,15 +1,7 @@
 import React from 'react';
 import Slider, { Rail, Handles, Tracks } from 'react-compound-slider';
 import Octicon from 'react-octicon';
-
-const railStyle = {
-  position: 'absolute',
-  width: '100%',
-  height: 10,
-  marginTop: 35,
-  borderRadius: 5,
-  backgroundColor: '#8B9CB6'
-};
+import "./VolumeSlider.css"
 
 const defaultValues = [50];
 
@@ -39,7 +31,7 @@ class VolumeSlider extends React.Component {
         <Rail>
           {(
             { getRailProps } // adding the rail props sets up events on the rail
-          ) => <div style={railStyle} {...getRailProps()} />}
+          ) => <div className="volume-slider-rail" {...getRailProps()} />}
         </Rail>
         <Handles>
           {({ handles, getHandleProps }) => (
@@ -72,20 +64,10 @@ function Handle({
 }) {
   return (
     <div
+      className="volume-slider-handle"
       style={{
         left: `${percent}%`,
-        position: 'absolute',
-        marginLeft: -5.5,
-        marginTop: 33,
-        zIndex: 2,
-        width: 15,
-        height: 15,
-        border: 0,
-        textAlign: 'center',
         cursor: disabled ? 'not-allowed' : 'pointer',
-        borderRadius: '50%',
-        backgroundColor: 'green',
-        color: '#333'
       }}
       {...getHandleProps(id)}
     />
@@ -96,16 +78,11 @@ function Track({ source, target, getTrackProps, disabled }) {
   // your own track component
   return (
     <div
+      className="volume-slider-track"
       style={{
-        position: 'absolute',
-        height: 10,
-        zIndex: 1,
-        marginTop: 35,
-        backgroundColor: '#145814',
-        borderRadius: 5,
-        cursor: disabled ? 'not-allowed' : 'pointer',
         left: `${source.percent}%`,
-        width: `${target.percent - source.percent}%`
+        width: `${target.percent - source.percent}%`,
+        cursor: disabled ? 'not-allowed' : 'pointer'
       }}
       {...getTrackProps()} // this will set up events if you want it to be clickeable (optional)
     />
