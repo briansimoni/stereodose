22dfe425819d0d9f5b533dff696feda75b6e6089
briansimoni <simonibc@vcu.edu>
2020-01-29 18:11:40 -0500

    Setup new schema

--- app/models/categories.go
+++ app/models/categories.go
@@ -2,6 +2,10 @@ package models
 
 // Categories
 type categories map[string][]string
+type categories2 []struct {
+	Name          string   `json:"name"`
+	Subcategories []string `json:"subcategories"`
+}
 
 // Categories is where the music genres are defined
 // We can use this to validate user input before performing database operations
@@ -11,6 +15,25 @@ var Categories = categories{
 	"ecstacy": []string{"dance", "floored", "rolling balls"},
 	"shrooms": []string{"matrix", "shaman", "space"},
 	"lsd":     []string{"calm", "trippy", "rockstar"},
+}
+
+var Categories2 = categories2{
+	{
+		Name:          "Weed",
+		Subcategories: []string{"chill", "groovin", "thug life"},
+	},
+	{
+		Name:          "ecstacy",
+		Subcategories: []string{"dance", "floored", "rolling balls"},
+	},
+	{
+		Name:          "shrooms",
+		Subcategories: []string{"matrix", "shaman", "space"},
+	},
+	{
+		Name:          "lsd",
+		Subcategories: []string{"calm", "trippy", "rockstar"},
+	},
 }
 
 func (c categories) Valid(category, subcategory string) bool {
