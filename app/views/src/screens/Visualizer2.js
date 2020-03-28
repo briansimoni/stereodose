c1a3972202edbb3807cfc26867bd8483d8075c8f
briansimoni <simonibc@vcu.edu>
2020-03-28 16:58:46 -0400

    double lines

--- app/views/src/screens/Visualizer2.js
+++ app/views/src/screens/Visualizer2.js
@@ -2,13 +2,13 @@ import React from 'react';
 import Spotify from 'spotify-web-api-js';
 
 export default class Visualizer2 extends React.Component {
-  analysis
-  features
-  playerState
-  currentTrackId // string spotify id
-  intervalTypes = ['tatums', 'segments', 'beats', 'bars', 'sections']
+  analysis;
+  features;
+  playerState;
+  currentTrackId; // string spotify id
+  intervalTypes = ['tatums', 'segments', 'beats', 'bars', 'sections'];
   /**
-   * 
+   *
    * @param {object} props
    * @param {App} props.app - a reference to the App component
    * @param {function} props.toggleVisualizer
@@ -16,8 +16,8 @@ export default class Visualizer2 extends React.Component {
   constructor(props) {
     super(props);
     this.ref = React.createRef();
-    this.canvas = document.createElement('canvas')
-    this.ctx = this.canvas.getContext('2d')
+    this.canvas = document.createElement('canvas');
+    this.ctx = this.canvas.getContext('2d');
     this.canvas.width = window.innerWidth;
     this.canvas.height = window.innerHeight;
 
@@ -27,14 +27,17 @@ export default class Visualizer2 extends React.Component {
     requestAnimationFrame(this.animate);
 
     this.x = 0;
-    this.y = 0;
+
+    this.volumePoint = new Point(0, window.innerHeight / 2 - 100);
+    this.pitchPoint = new Point(0, window.innerHeight / 2 - 50);
+
     this.sweeping = false;
   }
 
   resizeCanvas = () => {
     this.canvas.width = window.innerWidth;
     this.canvas.height = window.innerHeight;
-  }
+  };
 
   render() {
     return (
@@ -44,9 +47,11 @@ export default class Visualizer2 extends React.Component {
           nodeElement && nodeElement.appendChild(this.canvas);
         }}
       >
-        <button onClick={this.props.toggleVisualizer} id="close-visualizer-button" className="btn btn-danger">CLOSE</button>
+        <button onClick={this.props.toggleVisualizer} id="close-visualizer-button" className="btn btn-danger">
+          CLOSE
+        </button>
       </div>
-    )
+    );
   }
 
   // whenever the component mounts or the playback changes, we make sure to do the calls
@@ -54,20 +59,20 @@ export default class Visualizer2 extends React.Component {
   async componentDidMount() {
     const playerState = await this.props.app.player.getCurrentState();
     await this.getTrackInfo(playerState.track_window.current_track.id);
-    this.props.app.player.addListener('player_state_changed', this.playerStateChangeCallback)
+    this.props.app.player.addListener('player_state_changed', this.playerStateChangeCallback);
   }
 
-  playerStateChangeCallback = async (playerState) => {
+  playerStateChangeCallback = async playerState => {
     if (this.currentTrack !== playerState.track_window.current_track.id) {
       this.playerState = playerState;
       this.currentTrackId = playerState.track_window.current_track.id;
       await this.getTrackInfo(this.currentTrackId);
     }
-  }
+  };
 
   componentWillUnmount() {
     window.removeEventListener('resize', this.resizeCanvas);
-    this.props.app.player.removeListener('player_state_changed', this.playerStateChangeCallback)
+    this.props.app.player.removeListener('player_state_changed', this.playerStateChangeCallback);
   }
 
   animate = async () => {
@@ -81,32 +86,54 @@ export default class Visualizer2 extends React.Component {
     const segmentIndex = Math.floor((segments.length / songDuration) * playerState.position);
     const currentSegment = segments[segmentIndex];
     if (this.x >= window.innerWidth) {
-      this.ctx.closePath();
       this.x = -1;
-      this.y += 10;
-      this.ctx.beginPath();
+      this.volumePoint = new Point(0,0);
+      this.pitchPoint = new Point(0,0);
     }
-
 
     // TODO: remove this hot fix
     if (!currentSegment) {
       return;
     }
 
-    const y = (window.innerHeight / 2) - 100;
-    const amplitude = (Math.abs(currentSegment.loudness_start)) * 5
     this.sweep();
+    this.animateVolume(currentSegment);
+    this.animatePitch(currentSegment);
+    this.x += 1;
+  };
+
+  animateVolume = (segment) => {
+    const y = window.innerHeight / 2 - 100;
+    this.ctx.beginPath();
+    this.ctx.moveTo(this.volumePoint.x, this.volumePoint.y);
+    const amplitude = Math.abs(segment.loudness_start) * 5;
     this.ctx.lineTo(this.x, y + amplitude);
     this.ctx.strokeStyle = 'blue';
-    this.ctx.stroke()
-    this.x += 1;
-  }
+    this.volumePoint.x = this.x;
+    this.volumePoint.y = y + amplitude;
+    this.ctx.closePath();
+    this.ctx.stroke();
+  };
+
+  animatePitch = (segment) => {
+    console.log(this.analysis)
+    const y = window.innerHeight / 2 - 50;
+    this.ctx.beginPath();
+    this.ctx.moveTo(this.pitchPoint.x, this.pitchPoint.y);
+    const amplitude = Math.abs(segment.pitches[0]) * 50;
+    this.ctx.lineTo(this.x, y + amplitude);
+    this.ctx.strokeStyle = 'red';
+    this.pitchPoint.x = this.x;
+    this.pitchPoint.y = y + amplitude;
+    this.ctx.closePath();
+    this.ctx.stroke();
+  };
 
   sweep = () => {
     this.ctx.clearRect(this.x + 1, 0, 20, window.innerHeight);
-  }
+  };
 
-  getTrackInfo = async (trackId) => {
+  getTrackInfo = async trackId => {
     const accessToken = await this.props.app.getAccessToken();
     const spotify = new Spotify();
     spotify.setAccessToken(accessToken);
@@ -120,9 +147,18 @@ export default class Visualizer2 extends React.Component {
     } catch (err) {
       console.error(err);
     }
-  }
+  };
 
-  setActiveIntervals = () => {
+  setActiveIntervals = () => {};
+}
 
+class Point {
+  /**
+   * @param {number} x
+   * @param {number} y
+   */
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
   }
 }
