5eb9594e779c0630dc355962bd1ccfb5e00d10ed
briansimoni <simonibc@vcu.edu>
2020-05-03 23:44:27 -0400

    Add basic linear interpolation function

--- app/views/src/screens/Visualizer/Data3D.js
+++ app/views/src/screens/Visualizer/Data3D.js
@@ -5,6 +5,10 @@ import { GLTFLoader, GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
 
 
 export default class Data3D extends Visualizer {
+
+
+  segmentChanged = true // this is a flag that can be used later to check if the currentSegment changed or not
+
   /**
   *
   * @param {object} props
@@ -15,7 +19,7 @@ export default class Data3D extends Visualizer {
     super(props);
     this.ref = React.createRef();
 
-    this.loadJosiahsDoughnut().then((nut) => {
+    this.loadJosiahsDonut().then((nut) => {
       this.scene = new Three.Scene();
       this.camera = new Three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
@@ -31,7 +35,6 @@ export default class Data3D extends Visualizer {
       // let bigNut = new Three.Object3D();
       // bigNut = nut;
 
-      console.log(nut);
       this.scene.add(nut.scene);
       // this.scene.add(this.cube);
       this.camera.position.z = 5;
@@ -52,7 +55,7 @@ export default class Data3D extends Visualizer {
    * returns a Promise that resolves to a three js object
    * @returns {Promise<GLTF>}
    */
-  loadJosiahsDoughnut() {
+  loadJosiahsDonut() {
     return new Promise((resolve, reject) => {
       const loader = new GLTFLoader();
       loader.load('/public/three/blobvis.glb', (gltf, thing) => {
@@ -72,7 +75,7 @@ export default class Data3D extends Visualizer {
 
   async componentWillUnmount() {
     super.componentWillUnmount();
-    const nut = await this.loadJosiahsDoughnut();
+    const nut = await this.loadJosiahsDonut();
 
     this.scene.add(nut);
     window.removeEventListener('resize', this.resize, true);
@@ -106,15 +109,77 @@ export default class Data3D extends Visualizer {
   }
 
   animate = async () => {
-    requestAnimationFrame(this.animate);
-    if (this.nut) {
-      console.log(this.nut);
-      // this.nut.rotation.x += 0.01;
-      // this.nut.rotation.y += 0.01;
+    await this.setActiveIntervals();
+    let donut = null;
+    if (!this.nut) {
+      return;
     }
-    this.nut.scene.children[0].rotation.x += 0.01;
-    this.nut.scene.children[0].rotation.y += 0.02;
-    this.nut.scene.children[0].rotation.z += 0.03;
+    donut = this.nut.scene.children[0];
+    // if (donut.rotation.x < 0.5) {
+    //   donut.rotation.x += 0.01;
+    // }
+    donut.rotation.x += 0.001;
+    donut.rotation.y += 0.002;
+    donut.rotation.z += 0.003;
+
+    // scale the donut to the volume if the volume data is available.
+    if (this.currentSegment) {
+      const x = this.position;
+      let { loudness_start, start, duration } = this.currentSegment;
+      let loudness_end = this.nextSegment.loudness_start;
+      loudness_start = Math.abs(loudness_start);
+      loudness_end = Math.abs(loudness_end);
+
+      if (loudness_start !== this.placeholder) {
+        this.segmentChanged = true;
+      }
+
+      if (this.segmentChanged) {
+        this.placeholder = loudness_start; // used to detect if the segment changed
+        this.renderVolume = loudness_start; // mutated and used to render the donut scale
+        this.segmentChanged = false;
+      }
+
+      const x1 = start * 1000; // convert seconds to ms
+      const y1 = loudness_start;
+      const x2 = (start + duration) * 1000 // convert seconds to ms
+      const y2 = loudness_end;
+
+
+      const m = (y2 - y1) / (x2 - x1);
+      const b = y1 - (m * x1);
+
+      // console.log({
+      //   y: (m *x + b),
+      //   m,
+      //   x,
+      //   b,
+      //   x1,
+      //   y1,
+      //   x2,
+      //   y2,
+      //   duration,
+      //   renderVolume: this.renderVolume
+      // })
+
+      // console.log(x);
+
+      //y=mx+b
+      // (y2-y1)/(x2-x1) = m
+      //delta y/delta x = m
+      // console.log((m *x) + b);
+      // const volume = (m * x + b) // = y
+      const volume = 100 - (m * x + b);
+      console.log(volume);
+      donut.scale.x = Math.abs(volume) / 35;
+      donut.scale.y = Math.abs(volume) / 35;
+      donut.scale.z = Math.abs(volume) / 35;
+      // console.log('new scale x', donut.scale.x);
+
+    }
+
     this.renderer.render(this.scene, this.camera);
-  };
+    requestAnimationFrame(this.animate);
+  }
+
 }
--- app/views/src/screens/Visualizer/Visualizer.js
+++ app/views/src/screens/Visualizer/Visualizer.js
@@ -5,8 +5,12 @@ import './Visualizer.css'
 
 // props need to include an App instance
 export default class Visualizer extends React.Component {
-  analysis = null
-  features = null
+  analysis = undefined;
+  features = undefined;
+  currentSegment = null;
+  nextSegment = null;
+  currentSection = null;
+  fetchingData = true;
 
   getTrackInfo = async trackId => {
     const accessToken = await this.props.app.getAccessToken();
@@ -19,6 +23,7 @@ export default class Visualizer extends React.Component {
       ]);
       this.analysis = analysis;
       this.features = features;
+      this.fetchingData = false;
     } catch (err) {
       console.error(err);
     }
@@ -29,18 +34,56 @@ export default class Visualizer extends React.Component {
    * Thus we guarantee that every frame has the correct data available to it
    */
   setActiveIntervals = async () => {
-    if (!this.analysis || !this.features) {
-      await this.getTrackInfo();
+    if (this.fetchingData) {
+      console.log('fetching data');
+      return;
     }
-    console.log(this.analysis);
+
 
     const playerState = await this.props.app.player.getCurrentState();
     const songDuration = playerState.track_window.current_track.duration_ms;
-
+    const position = playerState.position;
+    const positionInSeconds = position / 1000;
     const segments = this.analysis.segments;
-    const segmentIndex = Math.floor((playerState.position * segments.length) / songDuration);
-    const currentSegment = segments[segmentIndex];
-    this.currentSegment = currentSegment;
+
+    // important to set the position everytime.
+    this.position = position;
+
+    // these loops and conditions helps to synchronize segments to the position
+    // this problem is hard to solve with proportions for some reason
+    if (this.currentSegment === null) {
+      // start at 1 because the first element's start is undefined
+      for(let i = 1; i < segments.length; i++) {
+        if (positionInSeconds > segments[i].start && positionInSeconds < segments[i+1].start) {
+          this.currentSegmentIndex = i;
+          break;
+        }
+      }
+      this.currentSegment = segments[this.currentSegmentIndex]; // n
+      this.nextSegment = segments[this.currentSegmentIndex + 1]; // n + 1
+    }
+
+    if (positionInSeconds >= this.nextSegment.start) {
+      this.currentSegmentIndex++;
+      this.currentSegment = segments[this.currentSegmentIndex]; // n
+      this.nextSegment = segments[this.currentSegmentIndex + 1]; // n + 1
+
+    }
+    // if (position >= this.nextSegment.start) {
+    //   console.log('bigger');
+    //   this.currentSegmentIndex++;
+    //   this.currentSegment = segments[this.currentSegmentIndex]; // n
+    //   this.nextSegment = segments[this.currentSegmentIndex + 1]; // n + 1
+    // }
+
+
+
+    // const segmentIndex = Math.floor((playerState.position * segments.length) / songDuration);
+    // const currentSegment = segments[segmentIndex];
+    // this.currentSegment = currentSegment;
+    // if (segments[segmentIndex +1] !== undefined) {
+    //   this.nextSegment = segments[segmentIndex+1];
+    // }
 
     const sections = this.analysis.sections;
     const sectionIndex = Math.floor((playerState.position * sections.length) / songDuration)
@@ -50,7 +93,8 @@ export default class Visualizer extends React.Component {
 
   async componentDidMount() {
     const playerState = await this.props.app.player.getCurrentState();
-    await this.getTrackInfo(playerState.track_window.current_track.id);
+    this.fetchingData = true;
+    this.getTrackInfo(playerState.track_window.current_track.id);
     this.props.app.player.addListener('player_state_changed', this.playerStateChangeCallback);
   }
 
