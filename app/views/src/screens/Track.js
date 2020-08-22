a182330b153b75871f11ba02880eca47fad4d584
briansimoni <simonibc@vcu.edu>
2020-08-21 22:28:32 -0400

    use spotify instead of stereodose to load tracks

--- app/views/src/screens/Track.js
+++ app/views/src/screens/Track.js
@@ -8,13 +8,18 @@ export default function Track(props) {
   const { track, onPlay } = props;
 
   // some math to get the track duration in minutes:seconds
-  const duration = track.duration / 1000;
+  const duration = track.duration_ms / 1000;
   const minutes = Math.floor(duration / 60);
   const seconds = Math.round(duration % 60);
   let displayTime = `${minutes}:${seconds}`;
   if (displayTime.split(':')[1].length === 1) {
     displayTime = `${minutes}:0${seconds}`;
   }
+
+  let artistsString = track.artists.reduce((str, artist) => {
+    return (str += artist.name + ', ');
+  }, '');
+  artistsString = artistsString.slice(0, artistsString.length - 2);
 
   // Apparently the Spotify ID from the web API doesn't necessarily match the ID in the web player
   // Sometimes there is linked_from object that contains the ID that you would find in the web API
@@ -23,24 +28,33 @@ export default function Track(props) {
     currentTrackId = props.currentlyPlayingTrack.linked_from.id || props.currentlyPlayingTrack.id;
   }
 
+  let thisTrackId;
+  if (track.linked_from) {
+    thisTrackId = track.linked_from.id;
+  } else {
+    thisTrackId = track.id;
+  }
+
   return (
     <div className="row">
       <div className="col-2">
         <button
           // added data elements to correlate to events in Google Tag Manager
-          data-track-id={track.spotifyID}
+          data-track-id={track.id}
           data-track-name={track.name}
           className="track-play-button btn"
           onClick={onPlay}
         >
-          {currentTrackId === track.spotifyID && !props.paused && <FontAwesomeIcon icon={faPause} />}
-          {((currentTrackId !== track.spotifyID) || props.paused) && <FontAwesomeIcon icon={faPlay} />}
+          {currentTrackId === thisTrackId && !props.paused && <FontAwesomeIcon icon={faPause} />}
+          {(currentTrackId !== thisTrackId || props.paused) && <FontAwesomeIcon icon={faPlay} />}
         </button>
       </div>
 
       <div className="col-8">
-        <h5 onClick={onPlay} className="track-name">{track.name}</h5>
-        <h6 className="artists">{track.artists}</h6>
+        <h5 onClick={onPlay} className="track-name">
+          {track.name}
+        </h5>
+        <h6 className="artists">{artistsString}</h6>
       </div>
 
       <div className="col-2">
